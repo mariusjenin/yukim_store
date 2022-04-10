485cfccc092394511b7dd72ad4c36e5ed173c83a
Benjamin Villa <ywqqqq@gmail.com>
2022-04-10 06:03:49 +0200

    First app build with few layouts

---
+++ YukimStore/app/src/main/java/com/example/yukimstore/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.yukimstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a1_register);
+    }
+}
