6d26b1dd2cbd59431e726536ebdf68f80b30634d
Benjamin Villa <ywqqqq@gmail.com>
2022-04-10 23:26:00 +0200

    update main layout

--- YukimStore/app/src/main/java/com/yukimstore/MainActivity.java
+++ YukimStore/app/src/main/java/com/yukimstore/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.m_add_product);
+        setContentView(R.layout.register);
     }
 }
