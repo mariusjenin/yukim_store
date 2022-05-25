b3d9ef2dd926cf96aa4bbac36cb155c9de8682dd
Benjamin Villa <ywqqqq@gmail.com>
2022-05-25 18:50:26 +0200

    Create store (without template)

--- YukimStore/app/src/main/java/com/yukimstore/activity/used/merchant/CreateStoreActivity.java
+++ YukimStore/app/src/main/java/com/yukimstore/activity/used/merchant/CreateStoreActivity.java
@@ -2,8 +2,10 @@ package com.yukimstore.activity.used.merchant;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.yukimstore.R;
@@ -11,34 +13,70 @@ import com.yukimstore.activity.ConnectedActivity;
 import com.yukimstore.activity.ConnectedMerchantActivity;
 import com.yukimstore.db.AppDatabase;
 import com.yukimstore.db.entity.Product;
+import com.yukimstore.db.entity.Store;
+import com.yukimstore.db.entity.User;
+import com.yukimstore.manager.ConnectionManager;
 
 public class CreateStoreActivity extends ConnectedMerchantActivity {
-    private AppDatabase db;
+    private ImageView templateChoice;
+    private String choice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.m_create_store_2);
-        db = AppDatabase.getInstance(getApplicationContext());
+        setContentView(R.layout.m_create_store);
+        templateChoice = findViewById(R.id.TemplateChoice);
+        choice = "clothing";
+    }
+
+    public void setTemplateToClothing(View view) {
+        templateChoice.setImageResource(R.drawable.template_clothing);
+        choice = "clothing";
+    }
+
+    public void setTemplateToSport(View view) {
+        templateChoice.setImageResource(R.drawable.template_sport);
+        choice = "sport";
+    }
+
+    public void setTemplateToFood(View view) {
+        templateChoice.setImageResource(R.drawable.template_food);
+        choice = "food";
+    }
+
+    public void setTemplateToSelfcare(View view) {
+        templateChoice.setImageResource(R.drawable.template_selfcare);
+        choice = "selfcare";
+    }
+
+    public void setTemplateToMusic(View view) {
+        templateChoice.setImageResource(R.drawable.template_music);
+        choice = "music";
+    }
+
+    public void setTemplateToTechnology(View view) {
+        templateChoice.setImageResource(R.drawable.template_technology);
+        choice = "technology";
     }
 
     public void createStore(View view) {
-        EditText et_pname = (EditText)findViewById(R.id.mcs2_productname);
-        EditText et_pdetails = (EditText)findViewById(R.id.mcs2_productdetails);
-        EditText et_pprice = (EditText)findViewById(R.id.mcs2_productprice);
+        EditText storeNameInput = findViewById(R.id.storeName);
+        String storeName = storeNameInput.getText().toString();
 
-        String pname = et_pname.getText().toString();
-        String pdetails = et_pdetails.getText().toString();
-        int pprice = Integer.parseInt(et_pprice.getText().toString());
-
-        if(pname.equals("") || pdetails.equals("") || pprice <= 0) {
-            Toast.makeText(this,"Please give coherent values for the product",Toast.LENGTH_SHORT).show();
+        if(storeName.equals("")) {
+            Toast.makeText(this,"Please enter a valid name",Toast.LENGTH_SHORT).show();
             return;
         }
 
-        Product product = new Product(pname,pdetails,pprice,0,null); //TODO
+        ConnectionManager cm = ConnectionManager.getInstance();
+        User user = cm.getUtilisateur();
 
-        db.productDAO().insert(product);
+        Store store = new Store(user.id_user,storeName,null);
+
+        AppDatabase db = AppDatabase.getInstance(getApplicationContext());
+        db.storeDAO().insert(store);
+
+        // TODO : Gérer les templates
 
         startActivity(new Intent(this,MerchantMenuActivity.class));
     }
